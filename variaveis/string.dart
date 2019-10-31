8fa51437ac3c7a105de23688b804c7baaf7d3798
josue <josue.lopes@ftdata.com.br>
2019-10-31 14:44:27 -0300

    trabalhando com String

---
+++ variaveis/string.dart
@@ -0,0 +1,8 @@
+main() {
+  String texto = 
+  "\nOlá este é um pequeno texto criado para demonstrar a utilização de String \n" + 
+  "no Dart com ele nós podemos armazenar uma cadeia de caracter, outro ponto \n" + 
+  "é que aqui não existe a variável Char, sendo somente utilizado a String \n" +
+  "para armazenar textos e letras. \n";
+  print(texto);
+}
