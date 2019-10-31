03c818a5e06de00bfce448fe0e89be5a93167c99
josue <josue.lopes@ftdata.com.br>
2019-10-31 14:50:36 -0300

    trabalhando com conversao

---
+++ variaveis/conversao.dart
@@ -0,0 +1,8 @@
+main() {
+  String pi = "3.1416";
+  double pi_double = double.parse(pi);
+  int pi_inteiro = pi_double.toInt();
+
+  print(pi_double.toString());
+  print(pi_inteiro.toString());
+}
